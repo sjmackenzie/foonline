871159c7328f535020a54b3275f2266a6ade12fe
codr4life <codr4life@gmail.com>
2017-03-05 23:37:10 +0100

    fixed output

--- foonline.lisp
+++ foonline.lisp
@@ -64,13 +64,13 @@
 
              (handler-case 
                  (with-input-from-string (in expr)
-                   (let ((out
-                           (with-output-to-string (out)
-                             (with-lifoo (:exec exec :output out)
-                               (lifoo-eval (lifoo-read :in in)
-                                           :exec exec)))))
-                     (unless (string= "" out)
-                       (html output (format nil "~a\\n" out)))))
+                   (with-lifoo (:exec exec)
+                     (let ((out
+                             (with-output-to-string (out)
+                               (setf (lifoo-output) out)
+                               (lifoo-eval (lifoo-read :in in)))))
+                       (unless (string= "" out)
+                         (html output (format nil "~a\\n" out))))))
                (error (e)
                  (html output (format nil "~a\\n" e)))))
            (html output
